305bd52910d315bacbfc18ca63fb6124dd9c31e7
daretobedifferent18 <daretobedifferent10920@gmail.com>
2021-07-07 23:49:52 +0530

    Code Cleanup 1

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// Dummy test - More test will be added later
 test('renders Quizeal (Dummy Test)', () => {
   render(<App />);
   const linkElement = screen.getByText(/uizeal/i);
