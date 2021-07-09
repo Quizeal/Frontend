bbb710204d4ac0583bbe2a12a7d52d9687cfa6ad
daretobedifferent18 <daretobedifferent10920@gmail.com>
2021-07-09 16:57:02 +0530

    Imrpove Design

--- src/components/extra/About.js
+++ src/components/extra/About.js
@@ -1,7 +1,34 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { Container, Divider, Grid, Grow, Typography } from '@material-ui/core';
 import React from 'react';
+import { Fragment, useState } from 'react';
+
+const useStyles = makeStyles((theme) => ({
+  divider: {
+    margin: theme.spacing(3),
+  },
+}));
 
 const About = () => {
-  return <div>About</div>;
+  const classes = useStyles();
+  return (
+    <Fragment>
+      <Grow in={true} timeout={1000}>
+        <Container component='main' maxWidth='md' spacing={5}>
+          <Typography variant='h4' align='center'>
+            About
+          </Typography>
+          <Divider className={classes.divider} />
+          <Typography variant='paragraph'>
+            The ultimate Quiz taking platform for the present times.
+          </Typography>
+        </Container>
+      </Grow>
+    </Fragment>
+  );
 };
 
 export default About;
+
+// TODO
+// --> ADD MORE DETAILED ABOUT PAGE
