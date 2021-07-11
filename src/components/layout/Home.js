9b64dce4331029615f65e03bfb1ae3be52ba0e1d
daretobedifferent18 <daretobedifferent10920@gmail.com>
2021-07-12 01:06:33 +0530

    Added Quiz Link Field (Home Page)

--- src/components/layout/Home.js
+++ src/components/layout/Home.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { Grid, Button, Typography } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Grid, Button, Typography, TextField } from '@material-ui/core';
+import { Link, Redirect } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import logo from '../../resources/logo.png';
 import home from '../../resources/home.gif';
+import { useHistory } from 'react-router';
 
 const useStyles = makeStyles((theme) => ({
   section: {
@@ -16,7 +17,16 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Home() {
+  const history = useHistory();
   const classes = useStyles();
+  const [quizCode, setQuizCode] = useState('');
+
+  const onChange = (e) => {
+    setQuizCode(e.target.value);
+  };
+  const goToQuiz = () => {
+    history.push(`/quiz/${quizCode}`);
+  };
 
   return (
     <Grid
@@ -48,15 +58,34 @@ export default function Home() {
               Get Started
             </Button>
           </Link>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              gap: '5px',
+            }}
+          >
+            <TextField
+              id='outlined-basic'
+              label='Add Quiz Code'
+              variant='outlined'
+              value={quizCode}
+              onChange={(e) => onChange(e)}
+              size='small'
+            />
+            <Button
+              color='primary'
+              variant='contained'
+              disabled={!Boolean(quizCode)}
+              onClick={goToQuiz}
+            >
+              Start
+            </Button>
+          </div>
         </Grid>
       </Grid>
-      <Grid
-        item
-        xs={12}
-        md={6}
-        className={classes.sectionRight}
-        // justifyContent='center'
-      >
+      <Grid item xs={12} md={6}>
         <Typography
           variant='h4'
           align='center'
